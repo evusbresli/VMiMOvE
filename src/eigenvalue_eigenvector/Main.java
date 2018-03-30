5476e7c0d0d498216c8c011c8ceabbd2aa7709a2
Andrey Markovski <ipurpleandrew@gmail.com>
2018-03-30 03:02:19 +0300

    beginning of lab 4

---
+++ src/eigenvalue_eigenvector/Main.java
@@ -0,0 +1,9 @@
+package eigenvalue_eigenvector;
+
+public class Main {
+    public static final double eps = 0.001;
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/eigenvalue_eigenvector/MatrixMultiplection.java
@@ -0,0 +1,39 @@
+package eigenvalue_eigenvector;
+
+public abstract class MatrixMultiplection {
+    public static void main(String[] args) {
+        double[][] mA =
+                {{-1, -6},
+                        {2, 6}};
+
+        double[][] mB =
+                {{2},
+                        {-1}};
+
+        double[][] res = calculate(mA, mB);
+
+        for (double[] re : res) {
+            for (int j = 0; j < res[0].length; j++) {
+                System.out.format("%6.3f ", re[j]);
+            }
+            System.out.println();
+        }
+    }
+
+    static double[][] calculate(double[][] mA, double[][] mB){
+        int m = mA.length;
+        int n = mB[0].length;
+        int o = mB.length;
+        double[][] res = new double[m][n];
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                for (int k = 0; k < o; k++) {
+                    res[i][j] += mA[i][k] * mB[k][j];
+                }
+            }
+        }
+
+        return res;
+    }
+}
