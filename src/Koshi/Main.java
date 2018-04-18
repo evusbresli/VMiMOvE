52bf574cc69ed60e38c5dd0cc5bcf5bc9e2875c5
Andrey Markovski <ipurpleandrew@gmail.com>
2018-04-18 16:08:38 +0300

    proceed lab 6

--- src/Koshi/Main.java
+++ src/Koshi/Main.java
@@ -5,17 +5,19 @@ import java.util.Scanner;
 public class Main {
     private static int ny;
 
-    private static double[] FPR(double x, double[] Y){
+    static double[] FPR(double x, double[] Y){
         double[] F = new double[ny];
         F[0] = Y[0] / x + Y[1] - Math.pow(Math.E, x);
         F[1] = (((2 * x) / Y[0]) + (Math.pow(Y[1], 2) / Math.pow(Math.E, x))) - 1;
-
+//        F[0] = Y[0] * ((1 / (2 * x)) - 1);
         return F;
     }
 
     private static void OUT(double x, double[] Y){
         System.out.printf("x=%.4f  y1=%.4f  u1=%.4f  d1=%.4f  y2=%.4f  u2=%.4f  d2=%.4f\n",
                 x, Y[0], 2 * x, (2 * x) - Y[0], Y[1], Math.pow(Math.E, x), Math.pow(Math.E, x) - Y[1]);
+//        System.out.printf("x=%.4f  y1=%.4f  u1=%.4f  d1=%.4f\n",
+//                x, Y[0], 2 * x, (2 * x) - Y[0]);
     }
 
     public static void main(String[] args) {
@@ -38,76 +40,87 @@ public class Main {
         System.out.print("\nВведите максимально допустимое количество итераций: ");
         //int nit = reader.nextInt();
         int nit = 10000;
-        System.out.print("\nВведите точность: ");
+        System.out.print("\nВведите точность: \n");
         //double eps = reader.nextDouble();
         double eps = 0.0001;
 
         double h = (b - a) / nx;
-        double x = a;
+        double X = a;
 
         double[] Y = new double[ny];
         Y[0] = a * 2; Y[1] = Math.pow(Math.E, a);
-        OUT(x, Y);
+        //Y[0] = 2.70;
+        OUT(X, Y);
+        double[] Y1 = Rungie_Kutt.calculate(X, Y, h, ny);
+        X += h;
+        OUT(X, Y1);
+        double[] Y2 = Rungie_Kutt.calculate(X, Y1, h, ny);
+        X += h;
+        OUT(X, Y2);
+        double[] Y3 = Rungie_Kutt.calculate(X, Y2, h, ny);
+        X += h;
+        OUT(X, Y3);
 
-        double[] Fm = FPR(x, Y);
 
-        double[] Yp = new double[ny];
-        for (int i = 0; i < ny; i++){
-            Yp[i] = Y[i] + ((h / 2) * Fm[i]);
-        }
-
-        x += h / 2;
-
-        double[] Fp1 = FPR(x, Yp);
-
-        for (int i = 0; i < ny; i++){
-            Yp[i] = Y[i] + ((h / 2) * Fp1[i]);
-        }
-
-        double[] Fp2 = FPR(x, Yp);
-
-        for (int i = 0; i < ny; i++){
-            Yp[i] = Y[i] + (h * Fp2[i]);
-        }
-
-        x += h / 2;
-
-        double[] Fp = FPR(x, Yp);
-
-        for (int i = 0; i < ny; i++){
-            Yp[i] = Y[i] + ((h / 6) * (Fm[i] + (2 * Fp1[i]) + (2 * Fp2[i]) + Fp[i]));
-        }
-
-        OUT(x, Y);
-
-        Fp = FPR(x, Yp);
-
-        for (int n = 2; n <= nx; n++){
-            for (int i = 0; i < ny; i++){
-                Y[i] = Yp[i] + ((h / 2) * ((3 * Fp[i]) - Fm[i]));
-            }
-
-            x += h;
-            double[] F = FPR(x, Y);
-            k = 0;
-            do {
-                k++; de = 0;
-                for (int i = 0; i < ny; i++){
-                    z = Yp[i] + ((h / 12) * ((5 * F[i]) + (8 * Fp[i]) + Fm[i]));
-                    d = Math.abs(z - Y[i]);
-                    Y[i] = z;
-                    if (d > de)
-                        de = d;
-                }
-
-                F = FPR(x, Y);
-            } while (k <= nit || de >= eps);
-
-            Yp = Y;
-            Fm = Fp;
-            Fp = F;
-
-            OUT(x, Y);
-        }
+//        double[] Fm = FPR(X, Y);
+//
+//        X += h / 2;
+//
+//        double[] Yp = new double[ny];
+//        for (int i = 0; i < ny; i++){
+//            Yp[i] = Y[i] + ((h / 2) * Fm[i]);
+//        }
+//
+//        double[] Fp1 = FPR(X, Yp);
+//
+//        for (int i = 0; i < ny; i++){
+//            Yp[i] = Y[i] + ((h / 2) * Fp1[i]);
+//        }
+//
+//        double[] Fp2 = FPR(X, Yp);
+//
+//        for (int i = 0; i < ny; i++){
+//            Yp[i] = Y[i] + (h * Fp2[i]);
+//        }
+//
+//        X += h / 2;
+//
+//        double[] Fp = FPR(X, Yp);
+//
+//        for (int i = 0; i < ny; i++){
+//            Yp[i] = Y[i] + h / 6 * (Fm[i] + (2 * Fp1[i]) + (2 * Fp2[i]) + Fp[i]);
+//        }
+//
+//        OUT(X, Y);
+//
+//        Fp = FPR(X, Yp);
+//
+//        for (int n = 2; n <= nx; n++){
+//            for (int i = 0; i < ny; i++){
+//                Y[i] = Yp[i] + h * (1.5 * Fp[i] - 0.5 * Fm[i]);
+//            }
+//
+//            X += h;
+//            double[] F = FPR(X, Y);
+//            k = 0;
+//            do {
+//                k++; de = 0;
+//                for (int i = 0; i < ny; i++){
+//                    z = Yp[i] + h * ((5 / 12 * F[i]) + (8 / 12 * Fp[i]) + (1 / 12 * Fm[i]));
+//                    d = Math.abs(z - Y[i]);
+//                    Y[i] = z;
+//                    if (d > de)
+//                        de = d;
+//                }
+//
+//                F = FPR(X, Y);
+//            } while (k <= nit || de >= eps);
+//
+//            Yp = Y;
+//            Fm = Fp;
+//            Fp = F;
+//
+//            OUT(X, Y);
+//        }
     }
 }
---
+++ src/Koshi/Rungie_Kutt.java
@@ -0,0 +1,34 @@
+package Koshi;
+
+public class Rungie_Kutt {
+
+    private static double f(double x, double y){
+        return 3 * Math.pow(x, 2) * y + Math.pow(x, 2) * Math.pow(Math.E, Math.pow(x, 3));
+    }
+
+    public static double[] calculate(double X, double[] Y, double h, int ny){
+        double[] Kn1 = Main.FPR(X, Y);
+        double[] Yp = new double[ny];
+        for (int i = 0; i < ny; i++){
+            Yp[i] = Y[i] + h/2*Kn1[i];
+        }
+        double[] Kn2 = Main.FPR(X + h/2, Yp);
+        for (int i = 0; i < ny; i++){
+            Yp[i] = Y[i] + h/2*Kn2[i];
+        }
+        double[] Kn3 = Main.FPR(X + h/2, Yp);
+        for (int i = 0; i < ny; i++){
+            Yp[i] = Y[i] + h*Kn3[i];
+        }
+        double[] Kn4 = Main.FPR(X + h, Yp);
+        for (int i = 0; i < ny; i++){
+            Yp[i] = (Kn1[i] + 2*Kn2[i] + 2*Kn3[i] + Kn4[i])/6;
+        }
+        double[] Yn = new double[ny];
+        for (int i = 0; i < ny; i++){
+            Yn[i] = Y[i] + h* Yp[i];
+        }
+
+        return Yn;
+    }
+}
